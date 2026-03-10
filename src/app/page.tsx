00df1a2f89195eaae09230299abe07694ae93f6e
Shamna K <shamnashami500@gmail.com>
2026-03-10 16:19:14 +0530

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,9 @@ export default function Home() {
        
     return (
         <div>
-<<<<<<< HEAD
             <h1  className='text-yellow-400'>This is my Updated Home Page</h1>
             <Image src={'/images/images.jpg'} alt="image" width={300} height={300}/><br></br>
             <Image src={'/images/download.jpg'} alt="image" width={300} height={300}/>
-=======
-            <h1  className='text-yellow-400'>This is Home Page</h1>
-            <Image src={'/images/images.jpg'} alt="image" width={300} height={300}/>
-            {/* <Image src={'/images/download.jpg'} alt="image" width={300} height={300}/> */}
->>>>>>> 45377e3 (Added one image)
         </div> 
     );
 }
